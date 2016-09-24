ddb66c8b8d1cdfd3484801224f85a66e7a77c83b
lentan <lentan1029@gmail.com>
2016-09-24 11:51:57 -0700

    changed grunt file to work with non-global installation of webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
         stderr: false
       },
       compile: {
-        command: 'webpack'
+        command: 'node ./node_modules/webpack/bin/webpack.js'
       },
       startDevServer: {
         command: 'node server.js'
