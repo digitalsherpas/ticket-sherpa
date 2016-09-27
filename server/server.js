93706f7a1c56efc7333e6103fcc0d7e79b6624bd
Andrew <andrewk17@gmail.com>
2016-09-27 16:38:19 -0700

    Remove Web3 references from server file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const webpackConfig = require('../webpack.config')
 const config = require('../config');
-
+const dbController = require('./database/dbController.js');
 const app = express();
 
 const webpack = require('webpack');
