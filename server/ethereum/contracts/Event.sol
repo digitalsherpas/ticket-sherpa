090449e98cefcb0603de6b4c1df0cd06103c576c
Andrew <andrewk17@gmail.com>
2016-09-27 13:57:33 -0700

    Implement Eventing

--- server/ethereum/contracts/Event.sol
+++ server/ethereum/contracts/Event.sol
@@ -7,34 +7,38 @@ contract Event {  // can be killed, so the owner gets sent the money in the end
   uint public numAttendees;
   uint public quota;
   uint public price;
-  string title;
+  string public title;
 
-  event PurchaseTicket(address _from, uint _amount); // so you can log the event
+  event PurchaseTicket(address _from, uint _amount, uint _numAttendees); // so you can log the event
   event RefundTicket(address _to, uint _amount); // so you can log the event
-  event MoneySent(uint _amount);
+  event InsufficientEther(uint _amountSent, uint _price);
+  event CreateEvent(address _organizer, uint _numAttendees, uint _quota, uint _price, string _title);
+  event ExceedQuota(uint _numAttendees, uint _quota);
 
-  function Event() { //TODO: add params to customize the event
+  function Event(string title, uint price, uint quota) { //TODO: add params to customize the event
     organizer = msg.sender;
-    title = 'Cool Event';
-    price = 10;
-    quota = 100;
+    title = title;
+    price = price;
+    quota = quota;
     numAttendees = 0;
+    CreateEvent(organizer, numAttendees, quota, price, title);
   }
 
   function buyTicket() payable {
     if (numAttendees > quota) {
+      ExceedQuota(numAttendees, quota);
       throw; // throw ensures funds will be returned
     }
 
-    if (msg.value != price) { //
-      MoneySent(msg.value);
+    if (msg.value < price) { //
+      InsufficientEther(msg.value, price);
       throw;
     }
 
     attendeesPaid[msg.sender] = true;
-    numAttendees++;
     if (!organizer.send(msg.value)) throw; //send ether but catch error
-    PurchaseTicket(msg.sender, msg.value);
+    numAttendees++;
+    PurchaseTicket(msg.sender, msg.value, numAttendees);
   }
 
   function getNumAttendees() constant returns (uint){
@@ -42,8 +46,8 @@ contract Event {  // can be killed, so the owner gets sent the money in the end
   }
 
   function verifyAttendee(address attendee) constant returns (bool) {
-    if (attendeesPaid[attendee]) { 
-      return true; 
+    if (attendeesPaid[attendee]) {
+      return true;
     } else {
       return false;
     }
