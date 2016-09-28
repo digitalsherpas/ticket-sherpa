e37ae4cf2de11190f0c579261ca8337ab93e3db3
Leonard Tan <lentan1029@gmail.com>
2016-09-27 18:58:24 -0700

    remove space

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 // renders logo and maps props
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
         <h1>Tickether</h1>
