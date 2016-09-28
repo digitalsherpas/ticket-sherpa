531d53b8df40ffdc1564a21d32cbfb89dc67db06
Chris Picato <christopherpicato@gmail.com>
2016-09-27 19:06:42 -0700

    Fix code for pull request

--- client/components/EventsList.jsx
+++ client/components/EventsList.jsx
@@ -5,14 +5,6 @@ export default class EventsListApp extends Component {
   render () {
     return (
       <div>
-        <h1>Hello</h1>
-        <h1>How's it going</h1>
-        <h1>It's working again</h1>
-        <h1>It's working again</h1>
-        <h1>It's working again</h1>
-        <h1>It's working again</h1>
-        <h1>It's working again</h1>
-        <h1>It's working again</h1>
         <EventsListContainer />
       </div>
     );
--- server/server.js
+++ server/server.js
@@ -6,30 +6,30 @@ const config = require('../config');
 const app = express();
 const opn = require('opn');
 
-var webpack = require('webpack'),
-    webpackDevMiddleware = require('webpack-dev-middleware'),
-    webpackHotMiddleware = require('webpack-hot-middleware'),
-    webpackconfig = require('../webpack.config.js'),
-    webpackcompiler = webpack(webpackconfig);
+const webpack = require('webpack'),
+  webpackDevMiddleware = require('webpack-dev-middleware'),
+  webpackHotMiddleware = require('webpack-hot-middleware'),
+  webpackconfig = require('../webpack.config.js'),
+  webpackcompiler = webpack(webpackconfig);
  
 //enable webpack middleware for hot-reloads in development
-function useWebpackMiddleware(app) {
-    app.use(webpackDevMiddleware(webpackcompiler, {
-        publicPath: webpackconfig.output.publicPath,
-        stats: {
-            colors: true,
-            chunks: false, // this reduces the amount of stuff I see in my terminal; configure to your needs
-            'errors-only': true
-        }
-    }));
-    app.use(webpackHotMiddleware(webpackcompiler, {
-        log: console.log
-    }));
- 
-    return app;
+let useWebpackMiddleware = (app) => {
+  app.use(webpackDevMiddleware(webpackcompiler, {
+    publicPath: webpackconfig.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: false, // this reduces the amount of stuff I see in my terminal; configure to your needs
+      'errors-only': true
+    }
+  }));
+  app.use(webpackHotMiddleware(webpackcompiler, {
+    log: console.log
+  }));
+
+  return app;
 }
 
-var http = require('http').Server(app);
+const http = require('http').Server(app);
 
 useWebpackMiddleware(app);
 
@@ -46,6 +46,4 @@ app.get('*', function(req, res) {
 });
 
 app.listen(config.SERVER_PORT);
-
-var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+opn('http://localhost:3000');
