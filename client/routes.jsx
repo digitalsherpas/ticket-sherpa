d295ac27a9f921d87fa8d03fa5f635935701d366
Chris Picato <christopherpicato@gmail.com>
2016-09-29 17:41:55 -0700

    Change host routes naming convention Make title a link

--- client/routes.jsx
+++ client/routes.jsx
@@ -6,21 +6,21 @@ import HostHome from './components/HostHome.jsx';
 import HostEventsContainer from './containers/hostEventsContainer.js';
 import HostCreateEventContainer from './containers/HostCreateEventContainer.js';
 import EventsListContainer from './containers/eventsListContainer.js';
+import EventDetailsContainer from './containers/EventDetailsContainer.js';
 import HostEventDetailsContainer from './containers/HostEventDetailsContainer.js';
 
-      // <Route path="/events/:eventName" component={EventDetailsContainer} />
 const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="/events" component={EventsListContainer} />
+      <Route path="/events/:eventName" component={EventDetailsContainer} />
 
-      <Route path="/host">
-        <IndexRoute component={HostHome} />
+      <Route path="/host" component={HostHome}>
         <Route path="/hostevents" component={HostEventsContainer} />
         <Route path="/hostcreateevent" component={HostCreateEventContainer} />
       </Route>
-      <Route path="/details/:hostEventName" component={HostEventDetailsContainer} />
+      <Route path="/hostEvents/:eventName" component={HostEventDetailsContainer} />
     </Route>
   </Router>
 );
