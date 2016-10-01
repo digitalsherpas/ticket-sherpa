3db7194dc2e465516b64b1e86534afb6945cbd81
lentan <lentan1029@gmail.com>
2016-09-30 22:18:15 -0700

    remove comments and console log

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@
 
 const express = require('express');
 const path = require('path');
-// const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-// const webpackConfig = require('../webpack.config')
 const config = require('../config');
 const opn = require('opn');
 const rp = require('request-promise');
