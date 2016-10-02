bff26c6958051ae90b39b9bf68e4723fa424db2f
Andrew <andrewk17@gmail.com>
2016-10-01 18:05:43 -0700

    Refactor of code to extract db server

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   WEBPACK_DEV_SERVER_PORT: 3001,
   SERVER_PORT: 3000,
+  ETH_SERVER_PORT: 3002,
   SECRET_OR_KEY: 'gustybanjo',
   SERVER_URL: 'http://localhost',
   AUTH_SERVER_PORT: 3003,
