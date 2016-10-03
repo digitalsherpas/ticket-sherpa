2408c016e605b116139604ab4aef5f4727143b35
lentan <lentan1029@gmail.com>
2016-10-02 17:25:42 -0700

    login redirect

--- client/components/Login/Login.jsx
+++ client/components/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Router } from 'react-router';
 
 // create a React Component to search through passed down events
 export default class Login extends Component {
@@ -15,14 +16,18 @@ export default class Login extends Component {
   }
 
   render() {
+    if (this.props.auth === 'complete') {
+      Router.transitionTo('/');
+    } else {
     return (
-      <form ref="loginForm" onSubmit={this.handleSubmit.bind(this)}>
-        <h4>Username</h4>
-        <input ref="username" type="text" />
-        <h4>Password</h4>
-        <input ref="password" type="password" />
-        <button type="submit">Submit</button>
-      </form>
-    );
+        <form ref="loginForm" onSubmit={this.handleSubmit.bind(this)}>
+          <h4>Username</h4>
+          <input ref="username" type="text" />
+          <h4>Password</h4>
+          <input ref="password" type="password" />
+          <button type="submit">Submit</button>
+        </form>
+      );
+    }
   }
 }
--- client/reducers/loginReducer.js
+++
@@ -1,1 +0,0 @@
-loginReducer.js
