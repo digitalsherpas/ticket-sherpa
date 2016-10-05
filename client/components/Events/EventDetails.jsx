bafb12e6996ace7658ca9423865f1d2b2bcb746e
Chris Picato <christopherpicato@gmail.com>
2016-10-05 12:48:21 -0700

    Reorder events and eventDetails pages

--- client/components/Events/EventDetails.jsx
+++ client/components/Events/EventDetails.jsx
@@ -3,19 +3,18 @@ import { Link } from 'react-router';
 
 const EventDetails = ({params: { eventName }, location: { query }}) => (
   <div>
+    <img src="http://tctechcrunch2011.files.wordpress.com/2008/04/linux-penguin-small.png" />
     <h4>Event Name: {eventName}</h4>
+    <p>(((Description Placeholder)))</p>
+    <p>Price: {query.price}</p>
     <p>Start Date:{query.eventStartDateTime}</p>
     <p>End Date:{query.eventEndDateTime}</p>
-    <p>Price: {query.price}</p>
-    <p>(((Description Placeholder)))</p>
     <p>(((Street Address Placeholder)))</p>
     <p>(((Address Line 2 Placeholder)))</p>
     <p>(((City Placeholder)))</p>
     <p>(((State Placeholder)))</p>
     <p>(((Zip/Postal Code Placeholder)))</p>
     <p>(((Country Placeholder)))</p>
-    <img src="https://images-na.ssl-images-amazon.com/images/G/01/aplusautomation/vendorimages/a6f8620a-9d8b-424b-b36a-eb326129a059.png._CB320335955__SR285,285_.png" />
-
     <h4><Link
       to={{ pathname:
         '/buyevent/' + eventName,
