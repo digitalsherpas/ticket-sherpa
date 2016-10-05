8b131731f8fe629754cc990beb8de2c10d656722
Chris Picato <christopherpicato@gmail.com>
2016-10-05 11:14:09 -0700

    Remove comments from tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -18,21 +18,7 @@ describe('components', () => {
   describe('App', () => {
     it('should render a link as the title', () => {
       const { enzymeWrapper } = setup();
-
       expect(enzymeWrapper.find('h1').text()).toBe('<Link />');
-
-      // const todoInputProps = enzymeWrapper.find('TodoTextInput').props()
-      // expect(todoInputProps.newTodo).toBe(true)
-      // expect(todoInputProps.placeholder).toEqual('What needs to be done?')
     });
-    //
-    // it('should call addTodo if length of text is greater than 0', () => {
-    //   const { enzymeWrapper, props } = setup()
-    //   const input = enzymeWrapper.find('TodoTextInput')
-    //   input.props().onSave('')
-    //   expect(props.addTodo.mock.calls.length).toBe(0)
-    //   input.props().onSave('Use Redux')
-    //   expect(props.addTodo.mock.calls.length).toBe(1)
-    // })
   });
 });
