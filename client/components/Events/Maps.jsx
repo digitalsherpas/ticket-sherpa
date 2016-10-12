a04b4527c8eca0a9a74d571138c87052d3351806
Chris Picato <christopherpicato@gmail.com>
2016-10-12 10:07:59 -0700

    Add link to info window

--- client/components/Events/Maps.jsx
+++ client/components/Events/Maps.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router';
 import Map, {GoogleApiWrapper} from 'google-maps-react';
 import keys from '../../../keys.js';
 import Marker from 'google-maps-react/dist/components/Marker.js';
+import InfoWindow from 'google-maps-react/dist/components/InfoWindow.js';
 // import InfoWindow from 'google-maps-react/dist/components/InfoWindow.js';
 // import SearchBar from './SearchBar/SearchBar.jsx';
 // import UserHome from './UserHome/UserHome.jsx';
@@ -11,6 +13,32 @@ import Marker from 'google-maps-react/dist/components/Marker.js';
 export class Container extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    }
+
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
+  }
+
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onMapClicked(props) {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
   }
 
   render() {
@@ -20,8 +48,28 @@ export class Container extends React.Component {
       .map((marker, index) => (
         <Marker
           key={index}
-          position={{lat: marker['latitude'], lng: marker['longitude']}} />
+          position={{lat: marker['latitude'], lng: marker['longitude']}}
+          onClick={this.onMarkerClick}
+          name={marker['eventName']}
+          description={marker['description']}
+          eventStartDateTime={marker['eventStartDateTime']}
+          eventEndDateTime={marker['eventEndDateTime']}
+          eventContractAddress={marker['eventContractAddress']}
+          price={marker['price']}
+          addressLine1={marker['addressLine1']}
+          addressLine2={marker['addressLine2']}
+          city={marker['city']}
+          state={marker['state']}
+          zipPostalCode={marker['zipPostalCode']}
+          country={marker['country']}
+          id={marker['id']}
+        />
       ));
+
+      // ${this.state.selectedPlace.eventContractAddress}&
+      // ${this.state.selectedPlace.price}&
+      // ${this.state.selectedPlace.id}
+      // Leg%20Day%20with%20Andrew?addressLine1=641+O%27Farrell+St&addressLine2=&city=San+Francisco&country=USA&description=Squats+for+days&eventEndDateTime=2016-11-21T18%3A00%3A00.000Z&eventName=Leg+Day+with+Andrew&eventStartDateTime=2016-11-20T18%3A00%3A00.000Z&state=CA&zipPostalCode=94109`
 
     return (
       <Map google={this.props.google}
@@ -31,6 +79,22 @@ export class Container extends React.Component {
         // center={{lat: 37.331686, lng: -122.030656}}
         >
         {Markers}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          className="such_awesome_class"
+          >
+          <div>
+            <h2>{this.state.selectedPlace.name}</h2>
+            {/* <a href={this.state.selectedPlace.linkUrl}>Link</a> */}
+            <Link href={
+              `/events/${this.state.selectedPlace.name}?addressLine1=${this.state.selectedPlace.addressLine1}&addressLine2=${this.state.selectedPlace.addressLine2}&city=${this.state.selectedPlace.city}&country=${this.state.selectedPlace.country}&description=${this.state.selectedPlace.description}&eventEndDateTime=${this.state.selectedPlace.eventEndDateTime}&eventName=${this.state.selectedPlace.name}&eventStartDateTime=${this.state.selectedPlace.eventStartDateTime}&state=${this.state.selectedPlace.state}&zipPostalCode=${this.state.selectedPlace.zipPostalCode}`
+              }
+            >
+              Event Details
+            </Link>
+          </div>
+        </InfoWindow>
       </Map>
     );
   }
@@ -39,3 +103,6 @@ export class Container extends React.Component {
 export default GoogleApiWrapper({
   apiKey: keys.GOOGLE_MAPS_API_KEY,
 })(Container);
+
+// {/* + 'description=' marker['description'] + 'eventStartDateTime=' + marker['eventStartDateTime'] + 'eventEndDateTime=' + marker['eventEndDateTime']} */}
+// {/* + marker['eventContractAddress'] + marker['price'] + marker['addressLine1'] + marker['addressLine2'] + marker['city'] + marker['state'] + marker['zipPostalCode'] + marker['country'] + marker['id']} */}
