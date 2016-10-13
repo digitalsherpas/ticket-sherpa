8b3765ae3f78b5dd17358be1b124d2c73021474a
Chris Picato <christopherpicato@gmail.com>
2016-10-12 18:25:28 -0700

    Resolve merge conflicts

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -8,19 +8,19 @@ export default class SignUp extends Component {
 
   render() {
     return (
-      <div className="signup__container">
-        <h3 className="signup__container-h2">Sign Up</h3>
-        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit.bind(this)}>
-          <p>Username</p>
-          <input type="text" ref="username" placeholder="Username"/>
-          <p>Password</p>
-          <input type="password" ref="password" placeholder="Password"/>
-          <p>Name</p>
-          <input type="text" ref="name" placeholder="e.g. John Doe"/>
-          <p>Email</p>
-          <input type="text" ref="email" placeholder="email@gmail.com"/>
-          <p>Phone Number</p>
-          <input type="text" ref="phonenumber" placeholder="e.g. +14702408888"/>
+      <div className="authentication__container">
+        <h3 className="authentication__container-header">Sign Up</h3>
+        <form className="authentication__content" ref="commentForm" onSubmit={this.handleSubmit.bind(this)}>
+          <p className="authentication__content-field">Username</p>
+          <input className="authentication__content-input" type="text" ref="username" placeholder="Username"/>
+          <p className="authentication__content-field">Password</p>
+          <input className="authentication__content-input" type="password" ref="password" placeholder="Password"/>
+          <p className="authentication__content-field">Name</p>
+          <input className="authentication__content-input" type="text" ref="name" placeholder="e.g. John Doe"/>
+          <p className="authentication__content-field">Email</p>
+          <input className="authentication__content-input" type="text" ref="email" placeholder="e.g. email@email.com"/>
+          <p className="authentication__content-field">Phone Number</p>
+          <input className="authentication__content-input" type="text" ref="phonenumber" placeholder="e.g. +14702408888"/>
           <input type="submit"/>
         </form>
       </div>
