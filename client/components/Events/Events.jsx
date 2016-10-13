87ad0ec8701cef69ef0022face883817f0d298fd
Chris Picato <christopherpicato@gmail.com>
2016-10-12 21:13:14 -0700

    Add padding to events header on events page

--- client/components/Events/Events.jsx
+++ client/components/Events/Events.jsx
@@ -30,7 +30,7 @@ export default class Events extends Component {
     (
       <div className="event__container-map-list">
       <div>
-        <h2>Events</h2>
+        <h2 className="map__events-header">Events</h2>
       </div>
       <div className="events__content">
 
