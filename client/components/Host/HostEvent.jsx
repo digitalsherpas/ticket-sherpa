8357dc8869d3008b2c1f62cbaf82e17b36932094
Andrew <andrewk17@gmail.com>
2016-10-14 12:22:47 -0700

    Fix event details page and search nav bar font size

--- client/components/Host/HostEvent.jsx
+++ client/components/Host/HostEvent.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router';
 import Moment from 'moment';
 
 const HostEvent = ({ eventName, eventStartDateTime }) => (
+  <Link to={`/hostevents/${eventName}`}>
   <div className="content__event-container">
-    <h4><Link to={`/hostevents/${eventName}`}>{eventName}</Link></h4>
+    <h4>{eventName}</h4>
     <p>{Moment(eventStartDateTime).format('MMM Do YYYY, h:mm A')}</p>
   </div>
+  </Link>
 );
 
 HostEvent.propTypes = {
