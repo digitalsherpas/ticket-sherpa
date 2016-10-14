63a13bb1700bffd87f544bd29e9a63a99c2c62af
Andrew <andrewk17@gmail.com>
2016-10-13 22:15:10 -0700

    Adjustments for presentation

--- client/components/Home/Home.jsx
+++ client/components/Home/Home.jsx
@@ -11,14 +11,18 @@ export default class Home extends Component {
       <div className="content">
         <div className="home__image-container">
           <div className="home__overlay">
-            <h1>
+            <h1 className="home__overlay-title">
               Sell Tickets
             </h1>
             <p>
               Manage your events through code. No middleman fees.
             </p>
+            <br/>
+            <p>
+              A decentralized event ticketing platform built on Ethereum.
+            </p>
           </div>
-          <img src={require('../../styles/images/eventbrite_homepage_image.jpg')} />
+          <img className="home__big-image" src={require('../../styles/images/eventbrite_homepage_image.jpg')} />
         </div>
         <div className="event__container">
           <div className="home__search">
